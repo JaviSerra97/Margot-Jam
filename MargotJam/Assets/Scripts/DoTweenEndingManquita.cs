afc2b7071e8ff6ec725d01c5a88b74b3147a216f
Lindryn <alomis1995@gmail.com>
2021-11-27 20:20:13 +0100

    Cupula

--- MargotJam/Assets/Scripts/DoTweenEndingManquita.cs
+++ MargotJam/Assets/Scripts/DoTweenEndingManquita.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class DoTweenStartingManquita : MonoBehaviour
+public class DoTweenEndingManquita : MonoBehaviour
 {
     private float _dotweenDur = 0.85f;
 
