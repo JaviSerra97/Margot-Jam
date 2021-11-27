3efa930400c1106127ac0076cd5168832138ab41
Lindryn <alomis1995@gmail.com>
2021-11-27 09:59:29 +0100

    VFX

---
+++ MargotJam/Assets/Scripts/DestroyerPS.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerPS : MonoBehaviour
+{
+    private ParticleSystem _ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ps = GetComponent<ParticleSystem>();
+        Destroy(gameObject, _ps.main.duration);
+    }
+}
