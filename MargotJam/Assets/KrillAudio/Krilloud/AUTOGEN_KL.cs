1f3a7571b5848a3653f75974cfbe677209d31280
jairo <magicjairo@gmail.com>
2021-11-27 18:40:57 +0100

    tags krillaudio

--- MargotJam/Assets/KrillAudio/Krilloud/AUTOGEN_KL.cs
+++ MargotJam/Assets/KrillAudio/Krilloud/AUTOGEN_KL.cs
@@ -7,11 +7,88 @@ namespace KrillAudio.Krilloud
 		public class Tags
 		{
 			/// <summary>
+			/// Tag: bg_sfx
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string bg_sfx = "bg_sfx";
+
+			/// <summary>
+			/// Tag: counter_sfx
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string counter_sfx = "counter_sfx";
+
+			/// <summary>
+			/// Tag: fail_sfx
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string fail_sfx = "fail_sfx";
+
+			/// <summary>
+			/// Tag: final_ost
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string final_ost = "final_ost";
+
+			/// <summary>
+			/// Tag: hover_button
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string hover_button = "hover_button";
+
+			/// <summary>
+			/// Tag: limits_sfx
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string limits_sfx = "limits_sfx";
+
+			/// <summary>
+			/// Tag: main_ost
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string main_ost = "main_ost";
+
+			/// <summary>
+			/// Tag: multiplier_sfx
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string multiplier_sfx = "multiplier_sfx";
+
+			/// <summary>
 			/// Tag: NewTag
 			/// 2D
 			/// </summary>
 			[System.Obsolete("Cache")]
 			public const string NewTag = "NewTag";
+
+			/// <summary>
+			/// Tag: perfect_sfx
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string perfect_sfx = "perfect_sfx";
+
+			/// <summary>
+			/// Tag: press_button
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string press_button = "press_button";
+
+			/// <summary>
+			/// Tag: touch_sfx
+			/// 2D
+			/// </summary>
+			[System.Obsolete("Placeholder")]
+			public const string touch_sfx = "touch_sfx";
 
 		}
 
