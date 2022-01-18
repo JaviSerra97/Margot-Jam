e74e981699a36cdbef598080c789eec37fd18672
Javier <jasercam97@gmail.com>
2022-01-18 18:36:04 +0100

    Menu a medias y Input System+

--- MargotJam/Assets/ManquitaBuilder/Scripts/Managers/MenuManager.cs
+++ MargotJam/Assets/ManquitaBuilder/Scripts/Managers/MenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,141 +8,62 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public static MenuManager Instance;
-
     [SerializeField] private Image fadeScreen;
+    [SerializeField] private float fadeDuration;
 
-    [Header("Buttons")]
-    [SerializeField] private CanvasGroup buttonsCanvasGroup;
-    [SerializeField] private float fallDuration = 0.5f;
-    [SerializeField] private float startYPos;
-    [SerializeField] private GameObject mainButtons;
-    private float mainButtonsPos;
-    [SerializeField] private GameObject logoImage;
-    private float logoImagePos;
-    [SerializeField] private Button exitButton;
-    private float exitButtonPos;
-    [SerializeField] private Button audioButton;
-    private float audioButtonPos;
-    [SerializeField] private Button creditsButton;
-    private float creditsButtonPos;
-
-    [Header("Credits")]
-    [SerializeField] private GameObject creditsPanel;
-    [SerializeField] private float creditsTweenDuration;
-    [SerializeField] private float creditsLockDuration;
-    private bool canExitCredits;
-    private bool exitCreditsInput;
-    private float timer;
+    private int selectedSceneIndex;
+    private bool canInteract;
 
     private void Awake()
     {
-        Instance = this;
-
-        mainButtonsPos = mainButtons.transform.position.y;
-        mainButtons.transform.position += new Vector3(0, startYPos, 0);
-
-        logoImagePos = logoImage.transform.position.y;
-        logoImage.transform.position += new Vector3(0, startYPos, 0);
-
-        exitButtonPos = exitButton.transform.position.y;
-        exitButton.transform.position += new Vector3(0, startYPos, 0);
-
-        audioButtonPos = audioButton.transform.position.y;
-        audioButton.transform.position += new Vector3(0, startYPos, 0);
-
-        creditsButtonPos = creditsButton.transform.position.y;
-        creditsButton.transform.position += new Vector3(0, startYPos, 0);
-
-        creditsPanel.transform.localScale = Vector3.zero;
+        fadeScreen.gameObject.SetActive(true);
     }
 
-    private void Update()
+    private void Start()
     {
-        if (canExitCredits)
-        {
-            if (Input.GetMouseButtonDown(0)) { exitCreditsInput = true; }
-
-            timer += Time.deltaTime;
-            if(timer > creditsLockDuration && exitCreditsInput)
-            {
-                ExitCredits();
-            }
-        }
-    }
-
-    public void ShowButtons()
-    {
-        Sequence seq = DOTween.Sequence();
-
-        //Fade + Caer jugar, tuto, creditos
-        seq.Append(fadeScreen.DOFade(0.5f, 0.5f));
-        seq.Join(creditsButton.transform.DOMoveY(creditsButtonPos, fallDuration).SetEase(Ease.InQuad));
-        seq.Join(mainButtons.transform.DOMoveY(mainButtonsPos, fallDuration).SetEase(Ease.InQuad));
-            
-
-        //Caer audio, salir
-        seq.Append(exitButton.transform.DOMoveY(exitButtonPos, fallDuration).SetEase(Ease.InQuad));
-        seq.Join(audioButton.transform.DOMoveY(audioButtonPos, fallDuration).SetEase(Ease.InQuad));
-
-        //Caer logo
-        seq.Append(logoImage.transform.DOMoveY(logoImagePos, fallDuration).SetEase(Ease.InQuad));
-
-        seq.Play();
+        FadeOut();
     }
 
     public void OnPlayButton()
     {
-        if (PlayerPrefs.GetFloat("ShowTutorial") == 1)
+        if (canInteract)
         {
-            //Jugar
-            LoadSceneByIndex(3);
-        }
-        else
-        {
-            //Tutorial
-            PlayerPrefs.SetFloat("ShowTutorial", 1);
-            PlayerPrefs.Save();
 
-            LoadSceneByIndex(2);
         }
     }
 
-    void LoadSceneByIndex(int index) { SceneManager.LoadScene(index); }
-
-    public void OnTutorialButton()
+    public void OnSettingsButton()
     {
-        LoadSceneByIndex(1);
+        if (canInteract)
+        {
+            
+        }
     }
 
-    public void OnExitButton()
+    void FadeIn()
     {
-        Application.Quit();
+        fadeScreen.DOFade(1, fadeDuration).SetEase(Ease.Linear).OnComplete(LoadScene).Play();
     }
 
-    public void OnCreditsButton()
+    void FadeOut()
     {
-        //Mostrar creditos
-        buttonsCanvasGroup.DOFade(0, creditsTweenDuration).SetEase(Ease.Linear).Play();
-        creditsPanel.transform.DOScale(1, creditsTweenDuration).SetEase(Ease.Linear).Play();
-
-        buttonsCanvasGroup.interactable = false;
-
-        canExitCredits = true;
+        fadeScreen.DOFade(0, fadeDuration).SetEase(Ease.Linear).OnComplete(AllowInteractions).Play();
     }
 
-    void ExitCredits()
+    public void LoadSceneByIndex(int i)
     {
-        creditsPanel.transform.DOScale(0, creditsTweenDuration).SetEase(Ease.Linear).Play();
-        buttonsCanvasGroup.DOFade(1, creditsTweenDuration).SetEase(Ease.Linear).OnComplete(AllowButtonsInteraction).Play();
-
-        canExitCredits = false;
-        exitCreditsInput = false;
-        timer = 0;
+        selectedSceneIndex = i;
+        FadeIn();
     }
 
-    void AllowButtonsInteraction()
+    void LoadScene()
     {
-        buttonsCanvasGroup.interactable = true;
+        SceneManager.LoadScene(selectedSceneIndex);
     }
+
+    void AllowInteractions()
+    {
+        canInteract = true;
+    }
+
 }
